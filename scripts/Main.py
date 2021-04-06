6d21bbd2c0ce71fa28407c88350734f0533293de
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2021-04-06 14:47:59 +0800

    Main窗口加入图片索引功能

--- scripts/Main.py
+++ scripts/Main.py
@@ -13,13 +13,25 @@ class Main:
         self.mainloop = self.root.mainloop
 
     def setProperty(self) -> None:
-        from tkinter import Canvas
-        from tkinter.ttk import LabelFrame, Combobox, Button, Label, Frame
+        from tkinter import Canvas, StringVar
+        from tkinter.ttk import LabelFrame, Combobox, Button, Label, Frame, Entry
 
-        self.root.bind("<a>", lambda event: self.pre())
-        self.root.bind("<Left>", lambda event: self.pre())
-        self.root.bind("<d>", lambda event: self.nxt())
-        self.root.bind("<Right>", lambda event: self.nxt())
+        self.indexLabel = Label(self.root, text="索引:")
+        self.indexVar = StringVar()
+        self.indexCheck = lambda strVar=self.indexVar, config=self.Config, key="INDEX": self.entryCallback(strVar, config, key)
+        self.indexCheck()
+        self.indexEntry = Entry(self.root, textvariable=self.indexVar, validate="focusout", validatecommand=self.indexCheck, width=10)
+        self.indexEntry.bind("<Return>", lambda e: (self.indexCheck(), self.applyImg()))
+        self.indexButton = Button(self.root, text="确认", command=self.applyImg)
+        self.indexLabel.grid(row=0, column=0, padx=3, pady=3)
+        self.indexEntry.grid(row=0, column=1, padx=3, pady=3)
+        self.indexButton.grid(row=0, column=2, padx=3, pady=3)
+
+        self.root.title("设置")
+        self.root.resizable(False, False)
+        self.root.attributes("-topmost", True)
+        self.root.attributes("-toolwindow", True)
+
         self.root.protocol("WM_DELETE_WINDOW", self._onClose)
 
     def readDir(self):
@@ -42,10 +54,26 @@ class Main:
             self.Config["INDEX"] += 1
             self.applyImg()
 
+    def delImg(self):
+        from os import remove
+
+        if len(self.names) != 0:
+            remove(self.names.pop(self.Config["INDEX"]))
+            self.applyImg()
+
+    def entryCallback(self, strVar, config: dict, key: str, Type=int):
+        try:
+            config[key] = Type(strVar.get())
+            return True
+        except:
+            strVar.set(str(config[key]))
+            return False
+
     def applyImg(self):
         from cv2 import imread
         from numpy import zeros
 
+        self.indexVar.set(str(self.Config["INDEX"]))
         self.Config["INDEX"] = max(0, min(self.Config["INDEX"], len(self.names) - 1))
         img = imread(self.Config["IMGDIR"] + self.names[self.Config["INDEX"]], 0) if self.names else zeros((self.Config["IMGPROCESS"]["N"], self.Config["IMGPROCESS"]["M"]), "uint8")
         self.imgWindow.setImg(img)
